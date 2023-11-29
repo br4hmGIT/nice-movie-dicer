251e409c0d9ebea2b0e7282f86ad3130dd871040
br4hm <haehnlein.alexander@gmx.de>
2023-11-29 22:41:49 +0100

    Per Pfeiltasten navigieren Funktion (works nearly well)

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 const input = document.getElementById('search');
 const dropdownList = document.getElementById('dropdown-list');
 const movieSuggest = document.getElementById('movieSuggest');
@@ -12,52 +11,13 @@ let filmYear = '';
 let filmImg = '';
 
 // API
-
 const options = {
     method: 'GET',
     headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ODdkOGE1YzViZWU3Njg1YzU5MWI5OWU1MGM2NmFhNyIsInN1YiI6IjY1NGZiOTc1NDFhNTYxMzM2ZDg4MWY1NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nRgZT0MEGk5Cv6OwExKNZSUq4a2wAF-8exzxTB17adU'
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ODdkOGE1YzViZWU3Njg1YzU5MWI5OWU1MGM2NmFhNyIsInN1YiI6IjY1NGZiOTc1NDFhNTYxMzM2ZDg4MWY1NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nRgZT0MEGk5Cv6OwExKNZSUq4a2wAF-8exzxTB17adU'
     }
-  };
-
-
-//  ++++++++++++++++
-
-// async function getMovie() {
-//     try {
-// 	const searchTerm = input.value;
-//     const searchUrl = `https://api.themoviedb.org/3/search/movie?query=${searchTerm}&include_adult=false&language=en-US&page=1`;
-
-//     const response = await fetch(searchUrl, options);
-
-//     console.log('Das ist meine Response: '+ response);
-
-//         if (!response.ok) {
-//                 alert(`${response.status}`);
-//         }
-
-//     const result = await response.json();
-//     console.log(result.results.slice(0, 5));
-
-//         if (result.results.length == 0) {
-//             alert(`No Film Found`);
-//             filmTitle = undefined;      
-//         }
-
-//     filmTitle = (result.results[0].original_title);
-//     filmYear = (result.results[0].release_date);
-//     filmImg = (result.results[0].poster_path);
-//     console.log(filmTitle);
-	
-//         } catch (error) {
-//             console.error(error);
-//         }
-// }
-
-
-//  ++++++++++++++++
-
+};
 
 let findMovieTitle, findMovieYear, findMovieImg, filmID;
 
@@ -66,144 +26,168 @@ async function findMovie() {
         const searchTerm = input.value;
         const searchUrl = `https://api.themoviedb.org/3/search/movie?query=${searchTerm}&include_adult=false&language=en-US&page=1`;
         const response = await fetch(searchUrl, options);
-    
-        console.log('Das ist meine Response: '+ response);
-    
-        
-    
+
+        console.log('Das ist meine Response: ' + response);
+
         const result = await response.json();
         result.results.slice(0, 5);
 
-        console.log('Das Result: ')
+        console.log('Das Result: ');
         console.log(result);
 
         dropdownList.innerHTML = '';
-        
-       
-        // const resultArray = Object.values(result);
+
         for (let i = 0; i < 5 && i < result.results.length; i++) {
             const obj = result.results[i];
-            // console.log('DIE SCHLEIFE:');
             filmTitle = obj.original_title;
             filmYear = obj.release_date.slice(0, 4);
             filmImg = obj.poster_path;
             filmID = obj.id;
-            
-        
-            // console.log(findMovieTitle);
-            // console.log(findMovieYear);
-            // console.log(findMovieImg);
-            
-            
+
             const listItem = document.createElement('li');
             listItem.classList.add('movieSuggest'); // Verwenden Sie eine Klasse statt einer ID
             listItem.innerHTML = `<img style="max-height:100px;" src="https://image.tmdb.org/t/p/w200/${filmImg}" alt=""><div>${filmTitle}, ${filmYear}</div>
-            <p id="movieID">${filmID}</p>
-            `
-           
-            ;
+            <p id="movieID">${filmID}</p>`;
+
             dropdownList.appendChild(listItem);
         }
-
-        
     } catch (error) {
         console.error(error);
     }
 }
 
+const addedMovieIds = new Set();
 
-    const addedMovieIds = new Set();
-
-    document.addEventListener('click', function (event) {
-          // Überprüfen, ob das geklickte Element oder eines seiner Eltern die Klasse 'movieSuggest' hat
-        const clickedItem = event.target.closest('.movieSuggest');
-        if (clickedItem) {
-        // Extrahiere Informationen aus dem HTML des geklickten Elements
+document.addEventListener('click', function (event) {
+    const clickedItem = event.target.closest('.movieSuggest');
+    if (clickedItem) {
         const clickedTitle = clickedItem.querySelector('div').textContent.split(',')[0].trim();
         const clickedYear = clickedItem.querySelector('div').textContent.split(',')[1].slice(1, 5);
         const clickedImg = clickedItem.querySelector('img').src;
         const clickedID = clickedItem.querySelector('p').textContent;
 
-        // console.log('DAS IST ES:' + clickedID);
-    
-            // Rufe die pushMovie-Funktion auf und übergebe die Informationen des geklickten Films
-         
-
-            if (!document.getElementById(clickedID)) {
-                pushMovie(clickedTitle, clickedYear, clickedImg, clickedID);
-            } else {
-                const movieElementSingle = document.getElementById(clickedID);
-                movieElementSingle.classList.add('alreadyAdded');
-                setTimeout(function() {
-                    // Entferne die Klasse nach dem Warten
-                    movieElementSingle.classList.remove('alreadyAdded');
-                }, 500);
-                
-            }
+        if (!document.getElementById(clickedID)) {
+            pushMovie(clickedTitle, clickedYear, clickedImg, clickedID);
+        } else {
+            const movieElementSingle = document.getElementById(clickedID);
+            movieElementSingle.classList.add('alreadyAdded');
+            setTimeout(function () {
+                movieElementSingle.classList.remove('alreadyAdded');
+            }, 500);
         }
-    });
+    }
+});
 
+async function pushMovie(title, year, img, id) {
+    const previousContent = filmEle.innerHTML;
 
-    async function pushMovie(title, year, img, id) {
-        const previousContent = filmEle.innerHTML;
-        
-        
-        const movieElement = document.createElement('div');
-        
-        movieElement.classList.add('movie');
-        
+    const movieElement = document.createElement('div');
 
-        movieElement.id = id;
-        movieElement.innerHTML = `<div class="remove-movie-button-wrapper"><button class="remove-movie-button">&#10006;</button></div>
-            <img id="moviePic" src="${img}" alt="">
-            <div class="movie-title">${title}</div>
-            <div class="movie-year">(${year})</div>`;
-        
-         movieElement.classList.add('fadeInAnimation');
+    movieElement.classList.add('movie');
 
-         filmEle.insertAdjacentHTML('beforeend', movieElement.outerHTML);
-        
-        setTimeout(function() {
-            movieElement.classList.remove('fadeInAnimation');
-        }, 200);    
-        
-        
-            
+    movieElement.id = id;
+    movieElement.innerHTML = `<div class="remove-movie-button-wrapper"><button class="remove-movie-button">&#10006;</button></div>
+        <img id="moviePic" src="${img}" alt="">
+        <div class="movie-title">${title}</div>
+        <div class="movie-year">(${year})</div>`;
+
+    movieElement.classList.add('fadeInAnimation');
+
+    filmEle.insertAdjacentHTML('beforeend', movieElement.outerHTML);
+
+    setTimeout(function () {
+        movieElement.classList.remove('fadeInAnimation');
+    }, 200);
+
     function removeClick(event) {
         const clearSingleMovieBtn = event.target.closest('.remove-movie-button');
         if (clearSingleMovieBtn) {
             const parentElement = clearSingleMovieBtn.closest('.movie');
-            
             parentElement.classList.add('fadeOutAnimation');
-            setTimeout(function() {
+            setTimeout(function () {
                 if (parentElement) {
                     parentElement.remove();
                 }
             }, 200);
-           
         }
     }
-        
+
     document.addEventListener('dblclick', function (event) {
-        handleButtonClick(event);
+        removeClick(event);
     });
 
-        document.addEventListener('click', function (event) {
-           removeClick(event);
-        });
-    }
-
-
+    document.addEventListener('click', function (event) {
+        removeClick(event);
+    });
+}
 
 // CLEAR-ALL BUTTON
-
 clearBtn.addEventListener('click', async function () {
-    
-    setTimeout(function() {
+    setTimeout(function () {
         filmEle.innerHTML = '';
     }, 200);
+});
+
+// Arrowkeys use
+
+
+
+
+
+let selectedIndex = -1;
+let clickedTitle = '';
+let clickedYear = '';
+let clickedImg = '';
+let clickedID = '';
+
+document.addEventListener('keydown', function (event) {
     
-   
+
+    const listItems = dropdownList.querySelectorAll('li');
+
+    if (event.key === 'ArrowDown') {
+        // Nach unten navigieren
+        selectedIndex = (selectedIndex + 1) % listItems.length;
+    } else if (event.key === 'ArrowUp') {
+        // Nach oben navigieren
+        selectedIndex = (selectedIndex - 1 + listItems.length) % listItems.length;
+    } else {
+        return; // Wenn keine Pfeiltaste gedrückt wurde, beende die Funktion
+    }
+
+    // Entferne zuvor hinzugefügte Klasse für aktive Auswahl
+    listItems.forEach(item => item.classList.remove('active-per-arrow'));
+
+    // Setze die Klasse für die ausgewählte Option
+    listItems[selectedIndex].classList.add('active-per-arrow');
+});
+
+// Füge einen Event-Listener für das Klicken auf die ausgewählte Option hinzu
+document.addEventListener('keydown', function (event) {
+    
+
+    if (event.key === 'Enter' && selectedIndex !== -1) {
+        event.preventDefault(); // Verhindere das Standardverhalten der Enter-Taste
+
+        const selectedElement = dropdownList.querySelectorAll('li')[selectedIndex];
+        // Setze die Werte für die ausgewählte Option
+        clickedTitle = selectedElement.querySelector('div').textContent.split(',')[0].trim();
+        clickedYear = selectedElement.querySelector('div').textContent.split(',')[1].slice(1, 5);
+        clickedImg = selectedElement.querySelector('img').src;
+        clickedID = selectedElement.querySelector('p').textContent;
+
+        // Führe die gewünschte Aktion für die ausgewählte Option aus
+        if (!document.getElementById(clickedID)) {
+            pushMovie(clickedTitle, clickedYear, clickedImg, clickedID);
+        } else {
+            const movieElementSingle = document.getElementById(clickedID);
+            movieElementSingle.classList.add('alreadyAdded');
+            setTimeout(function () {
+                movieElementSingle.classList.remove('alreadyAdded');
+            }, 500);
+        }
+        // console.log('Ausgewählte Option:', selectedElement.textContent, clickedTitle, clickedYear, clickedImg, clickedID);
+    }
 });
 
 
